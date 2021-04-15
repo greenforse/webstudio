0b3fbc0aad3dca0fe764142785b4b3e6082668c8
Kosyuchenko Anton <greenforse1996@gmail.com>
2021-04-15 18:55:40 +0500

    dva

--- Department.py
+++ Department.py
@@ -5,17 +5,40 @@ class Departament(Observer):
         self.workerList=[]
         self.observers=[]
         self.worklist=[]
+        self.chief = None
     def addWorker(self,worker):
-        self.workerList.append(worker)
+        if worker.chief == False:
+            self.workerList.append(worker)
+        else:
+            print("начальник не может быть обычным сотрудником")
+    def addChief(self,chief):
+        self.chief = chief
 
     def subscribe(self,observer):
         self.observers.append(observer)
 
     def unsubcribe(self,observer):
-
+        for i in range(len(self.observers)):
+            self.observers[i]=observer
+            del self.observers[i]
     def notify(self):
         for observer in self.observers:
             observer.run()
 
-    def run(self,):
+    def run(self):
+
+    def selectWorker(self):
+        ready= False
+        for worker in self.workerList:
+            if worker.run == False :
+                worker.taskRun(day)
+                ready=True
+        if ready == False and self.chief.run == False:
+            self.chief.taskRun(day):
+            ready = True
+        if ready == False: 
+            pass
+            
+
+
         
--- worker.py
+++ worker.py
@@ -2,19 +2,24 @@ from abc import abstractmethod
 from abc import ABCMeta, abstractmethod
 import random
 
-class Worker(metaclass=abstractmethod):
-    def __init__(self,name,chief):
+class Worker():
+    def __init__(self,name,chief,departament):
         self.completeChance = random.randint(70,95)
         self.chief=bool(chief)
         self.name=name
         self.score=0
+        self.departament=departament
+        self.run = False
 
-    def taskRun(self):
-        if random.randint(1,100) > self.completeChance:
+    def taskRun(self,day):    #поставил переменную day, планирую в  мейне(вайле)  вызывать каждый раз таскран и под конеч выдвать результат работы
+        taskDays +=1
+        complete=False
+        if taskDays == day and random.randint(1,100) > self.completeChance:
             print("Выполнено в срок")
+            complete=True
             self.score+=1
-        else:
-            self.score-=1
-
+        if complete == False:
+            self.run = True
+        else: self.run = False
     def getScore(self):
         return self.score
